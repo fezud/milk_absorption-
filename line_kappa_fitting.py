5489ae80b7800cc7e19d6006552beea75cc16802
fezud <fezudd@yandex.ru>
2022-10-26 19:41:36 +0300

    Initial beer commit

--- line_kappa_fitting.py
+++ line_kappa_fitting.py
@@ -11,23 +11,24 @@ ws = wb.active
 def linear(x, k):
     return k*x
 
-concentrations = np.array([1/300, 1/400, 1/600, 1/800, 1/1600])
+concentrations = np.array([1/20, 1/25, 1/30, 1/50])
 
 wavelengths = []
 kappas = []
 
 for row in ws.iter_rows(min_row=2):
     wavelength = float(str(row[0].value).replace(',', '.'))
-    kappa_values = np.array([float(str(raw_cell_data.value).replace(',', '.')) for raw_cell_data in row[1:]])
+    kappa_values = np.array([float(str(raw_cell_data.value).replace(',', '.')) if float(str(raw_cell_data.value).replace(',', '.')) > 0 else 0 for raw_cell_data in row[9:]])
 
     pars, _ = curve_fit(f=linear, xdata=concentrations, ydata=kappa_values)
+    print(wavelength, pars[0])
     wavelengths.append(wavelength)
     kappas.append(pars[0])
 
 output_wb = Workbook()
 output_ws = output_wb.active
 
-output_ws.title = "Milk Kappa"
+output_ws.title = "Beer Kappa"
 
 for i in range(len(wavelengths)):
     output_ws.append((wavelengths[i], kappas[i]))
